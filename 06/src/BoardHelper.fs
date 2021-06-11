1b584d1fe6b644820155d0b50d81135b94a7370a
pacokwon <haechank@gmail.com>
2021-06-11 19:46:00 +0900

    feat: 06

--- 06/src/BoardHelper.fs
+++ 06/src/BoardHelper.fs
@@ -1,7 +1,52 @@
 module CS220.BoardHelper
 
-let checkWinner states =
-  failwith "Fill Me"
+let checkWinner (states: SlotState[]) =
+  let getEncoded foo =
+    match foo with
+    | EmptySlot -> 0
+    | Marked (O) -> 1
+    | Marked (X) -> -1
+
+  let sumHorizontal idx =
+    [states.[idx]; states.[idx + 1]; states.[idx + 2]]
+    |> List.map getEncoded
+    |> List.reduce (+)
+
+  let sumVertical idx =
+    [states.[idx]; states.[idx + 3]; states.[idx + 6]]
+    |> List.map getEncoded
+    |> List.reduce (+)
+
+  let sumMajor =
+    [states.[0]; states.[4]; states.[8]]
+    |> List.map getEncoded
+    |> List.reduce (+)
+
+  let sumMinor =
+    [states.[2]; states.[4]; states.[6]]
+    |> List.map getEncoded
+    |> List.reduce (+)
+
+  [sumHorizontal 0; sumHorizontal 3; sumHorizontal 6; sumVertical 0; sumVertical 1; sumVertical 2; sumMajor; sumMinor]
+  |> List.fold (fun acc sum ->
+    if acc <> None then acc else
+    match sum with
+    | 3 -> Some O
+    | -3 -> Some X
+    | _ -> None
+  ) None
 
 let isDraw states =
-  failwith "Fill Me"
+  let isFull =
+    Array.fold (
+      fun full state ->
+        if not full then full else
+        match state with
+        | EmptySlot -> false
+        | _ -> true
+    ) true states
+
+  if not isFull then false else
+  match checkWinner states with
+  | None -> true
+  | _ -> false
--- 06/src/Minimax.fs
+++ 06/src/Minimax.fs
@@ -4,4 +4,33 @@ namespace CS220
 type MinimaxStrategy () =
   inherit AI ()
     override __.NextMove player board =
-      failwith "Fill Me"
+      let getall idx acc elt =
+        idx :: acc
+
+      let folder idx acc elt =
+        match elt with
+        | EmptySlot -> idx :: acc
+        | _ -> acc
+
+      let rec getScore currentPlayer (board: Board) =
+        let emptyIdx = board.Fold folder []
+
+        if List.isEmpty emptyIdx then
+          match board.CheckWinner () with
+          | None -> (0, 0)
+          | Some m when m = player -> (0, 1) // when player wins
+          | _ -> (0, -1) // when player loses
+        else
+
+        let scores = List.map (fun idx ->
+          let newBoard = board.Copy ()
+          newBoard.Mark idx currentPlayer |> ignore;
+          (getScore <| Marker.getOpponent currentPlayer <| newBoard) |> snd) emptyIdx
+
+        let desiredScore = scores |> (if currentPlayer = player then List.max else List.min)
+        match List.tryFindIndex (fun score -> score = desiredScore) scores with
+        | Some idx -> (emptyIdx.[idx], desiredScore)
+        | None -> failwith "Should not happen"
+
+      let (idx, score) = getScore player board
+      idx
